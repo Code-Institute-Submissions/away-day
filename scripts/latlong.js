a1a16ac03c614c6b64c819aaf6e8b71df766e0a8
Stuart Saint <stuartsaint242@gmail.com>
2020-10-04 16:08:49 +0000

    File creation: data required

---
+++ scripts/latlong.js
@@ -0,0 +1,681 @@
+/* Bundesliga */
+
+let bunArmBie = {
+    lat: 52.031920,
+    lng: 8.516527
+}
+
+let bunAugsburg = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let bunBayerL = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let bunBayern = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let bunBorDor = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let bunBorMon = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let bunEinFra = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let bunFreiburg = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let bunHerthaB = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let bunHoffen = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let bun1FCKln = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let bunMainz05 = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let bunLeipzig = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let bunSchalke = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let bunVFBStu = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let bunUnionB = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let bunWerBre = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let bunVFLWol = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+/* Championship */
+
+let flcBarnsley = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let flcBirCty = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let flcBlaRov = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let flcBourne = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let flcBrentf = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let flcBriCty = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let flcCarCty = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let flcCovCty = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let flcDerCou = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let flcHudTow = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let flcLutTow = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let flcMiBoro = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let flcMillwall = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let flcNorCty = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let flcNotFor = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let flcPreston = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let flcQPRang = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let flcReading = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let flcRotUtd = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let flcSheWed = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let flcStoCty = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let flcSwaCty = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let flcWatford = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let flcWycWan = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+/* GAA*/
+
+let gaaAntrim = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let gaaArmagh = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let gaaCarlow = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let gaaCavan = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let gaaClare = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let gaaCork = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let gaaDerry = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let gaaDonegal = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let gaaDown = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let gaaDublin = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let gaaFermanagh = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let gaaGalway = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let gaaKerry = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let gaaKildare = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let gaaKilkenny = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let gaaLaois = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let gaaLeitrim = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let gaaLimerick = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let gaaLongford = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let gaaLouth = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let gaaMayo = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let gaaMeath = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let gaaMonaghan = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let gaaOffaly = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let gaaRoscommon = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let gaaSligo = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let gaaTipperary = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let gaaTyrone = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let gaaWaterford = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let gaaWestmeath = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let gaaWexford = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let gaaWicklow = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+/* NHL*/
+
+let nhlDucks = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let nhlCoyotes = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let nhlBruins = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let nhlSabres = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let nhlFlames = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let nhlHurricanes = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let nhlAvalanche = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let nhlBlackhawks = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let nhlJackets = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let nhlStars = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let nhlOilers = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let nhlPanthers = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let nhlKings = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let nhlWild = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let nhlCanadiens = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let nhlDevils = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let nhlIslanders = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let nhlRangers = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let nhlOttawa = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let nhlFlyers = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let nhlPenguins = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let nhlSharks = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let nhlBlues = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let nhlLightning = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let nhlLeafs = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let nhlCanucks = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let nhlKnights = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let nhlCapitals = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let nhlJets = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+/* Premier League*/
+
+let eplArsenal = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let eplAstVil = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let eplBriAlb = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let eplBurnley = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let eplChelsea = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let eplEverton = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let eplFulham = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let eplLeeUtd = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let eplLeiCty = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let eplLiverp = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let eplManCty = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let eplManUtd = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let eplNewUtd = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let eplCryPal = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let eplSheUtd = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let eplSoutha = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let eplTSpurs = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let eplWestBrom = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let eplWestHam = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let eplWolves = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+/* Super League*/
+
+let slrCasTig = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let slrCatDra = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let slrHudGia = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let slrHullFC = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let slrHullKR = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let slrLeeRhi = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let slrStHlns = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let slrSalRed = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let slrWakTri = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let slrWarWol = {
+    lat: 99.999999,
+    lng: 99.999999
+}
+
+let slrWigWar = {
+    lat: 99.999999,
+    lng: 99.999999
+}
