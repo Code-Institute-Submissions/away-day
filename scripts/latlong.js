1262d1decd971bf1208c6481cd9b5d53ba069e92
Stuart Saint <stuartsaint242@gmail.com>
2020-10-06 12:09:01 +0000

    Updated: lat-long refs

--- scripts/latlong.js
+++ scripts/latlong.js
@@ -75,99 +75,99 @@ let bunVFLWol = {
 /* Championship */
 
 let flcBarnsley = {
-    lat: 99.999999, lng: 99.999999
+    lat: 53.552195, lng: -1.467781
 }
 
 let flcBirCty = {
-    lat: 99.999999, lng: 99.999999
+    lat: 52.475680, lng: -1.868044
 }
 
 let flcBlaRov = {
-    lat: 99.999999, lng: 99.999999
+    lat: 53.728503, lng: -2.489116
 }
 
 let flcBourne = {
-    lat: 99.999999, lng: 99.999999
+    lat: 50.735213, lng: -1.838267
 }
 
 let flcBrentf = {
-    lat: 99.999999, lng: 99.999999
+    lat: 51.490683, lng: -0.288730
 }
 
 let flcBriCty = {
-    lat: 99.999999, lng: 99.999999
+    lat: 51.440148, lng: -2.620163
 }
 
 let flcCarCty = {
-    lat: 99.999999, lng: 99.999999
+    lat: 51.472770, lng: -3.203034
 }
 
 let flcCovCty = {
-    lat: 99.999999, lng: 99.999999
+    lat: 52.475680, lng: -1.868044
 }
 
 let flcDerCou = {
-    lat: 99.999999, lng: 99.999999
+    lat: 52.914937, lng: -1.447248
 }
 
 let flcHudTow = {
-    lat: 99.999999, lng: 99.999999
+    lat: 53.654194, lng: -1.768365
 }
 
 let flcLutTow = {
-    lat: 99.999999, lng: 99.999999
+    lat: 51.884147, lng: -0.431487
 }
 
 let flcMiBoro = {
-    lat: 99.999999, lng: 99.999999
+    lat: 54.578218, lng: -1.216750
 }
 
 let flcMillwall = {
-    lat: 99.999999, lng: 99.999999
+    lat: 51.485908, lng: -0.050878
 }
 
 let flcNorCty = {
-    lat: 99.999999, lng: 99.999999
+    lat: 52.622028, lng: 1.309118
 }
 
 let flcNotFor = {
-    lat: 99.999999, lng: 99.999999
+    lat: 52.939948, lng: -1.132911
 }
 
 let flcPreston = {
-    lat: 99.999999, lng: 99.999999
+    lat: 53.772229, lng: -2.688216
 }
 
 let flcQPRang = {
-    lat: 99.999999, lng: 99.999999
+    lat: 51.509319, lng: -0.232108
 }
 
 let flcReading = {
-    lat: 99.999999, lng: 99.999999
+    lat: 51.422398, lng: -0.982545
 }
 
 let flcRotUtd = {
-    lat: 99.999999, lng: 99.999999
+    lat: 53.426874, lng: -1.362421
 }
 
 let flcSheWed = {
-    lat: 99.999999, lng: 99.999999
+    lat: 53.411400, lng: -1.500750
 }
 
 let flcStoCty = {
-    lat: 99.999999, lng: 99.999999
+    lat: 52.988429, lng: -2.175439
 }
 
 let flcSwaCty = {
-    lat: 99.999999, lng: 99.999999
+    lat: 51.642683, lng: -3.934559
 }
 
 let flcWatford = {
-    lat: 99.999999, lng: 99.999999
+    lat: 51.649876, lng: -0.401648
 }
 
 let flcWycWan = {
-    lat: 99.999999, lng: 99.999999
+    lat: 51.630529, lng: -0.800300
 }
 
 /* GAA*/
@@ -193,11 +193,11 @@ let gaaClare = {
 }
 
 let gaaCork = {
-    lat: 99.999999, lng: 99.999999
+    lat: 51.899744, lng: -8.435278
 }
 
 let gaaDerry = {
-    lat: 99.999999, lng: 99.999999
+    lat: 54.993008, lng: -7.333548
 }
 
 let gaaDonegal = {
@@ -209,7 +209,7 @@ let gaaDown = {
 }
 
 let gaaDublin = {
-    lat: 99.999999, lng: 99.999999
+    lat: 53.360496, lng: -6.251235
 }
 
 let gaaFermanagh = {
@@ -273,7 +273,7 @@ let gaaRoscommon = {
 }
 
 let gaaSligo = {
-    lat: 99.999999, lng: 99.999999
+    lat: 54.257355, lng: -8.465722
 }
 
 let gaaTipperary = {
@@ -285,7 +285,7 @@ let gaaTyrone = {
 }
 
 let gaaWaterford = {
-    lat: 99.999999, lng: 99.999999
+    lat: 52.254657, lng: -7.128762
 }
 
 let gaaWestmeath = {
@@ -421,127 +421,179 @@ let nhlJets = {
 /* Premier League*/
 
 let eplArsenal = {
-    lat: 99.999999, lng: 99.999999
+    lat: 51.555119, lng: -0.108315
 }
 
 let eplAstVil = {
-    lat: 99.999999, lng: 99.999999
+    lat: 52.509344, lng: -1.885077
 }
 
 let eplBriAlb = {
-    lat: 99.999999, lng: 99.999999
+    lat: 50.861666, lng: -0.083798
 }
 
 let eplBurnley = {
-    lat: 99.999999, lng: 99.999999
+    lat: 53.788920, lng: -2.230139
 }
 
 let eplChelsea = {
-    lat: 99.999999, lng: 99.999999
-}
-
-let eplEverton = {
-    lat: 99.999999, lng: 99.999999
-}
-
-let eplFulham = {
-    lat: 99.999999, lng: 99.999999
-}
-
-let eplLeeUtd = {
-    lat: 99.999999, lng: 99.999999
-}
-
-let eplLeiCty = {
-    lat: 99.999999, lng: 99.999999
-}
-
-let eplLiverp = {
-    lat: 99.999999, lng: 99.999999
-}
-
-let eplManCty = {
-    lat: 99.999999, lng: 99.999999
-}
-
-let eplManUtd = {
-    lat: 99.999999, lng: 99.999999
-}
-
-let eplNewUtd = {
-    lat: 99.999999, lng: 99.999999
+    lat: 51.481711, lng: -0.190867
 }
 
 let eplCryPal = {
-    lat: 99.999999, lng: 99.999999
+    lat: 51.398206, lng: -0.085412
+}
+
+let eplEverton = {
+    lat: 53.438699, lng: -2.966390
+}
+
+let eplFulham = {
+    lat: 51.474689, lng: -0.221389
+}
+
+let eplLeeUtd = {
+    lat: 53.777766, lng: -1.572214
+}
+
+let eplLeiCty = {
+    lat: 52.620248, lng: -1.142294
+}
+
+let eplLiverp = {
+    lat: 53.430797, lng: -2.960969
+}
+
+let eplManCty = {
+    lat: 53.483032, lng: -2.200471
+}
+
+let eplManUtd = {
+    lat: 53.463050, lng: -2.291088
+}
+
+let eplNewUtd = {
+    lat: 54.975505, lng: -1.621579
 }
 
 let eplSheUtd = {
-    lat: 99.999999, lng: 99.999999
+    lat: 53.370407, lng: -1.470744
 }
 
 let eplSoutha = {
-    lat: 99.999999, lng: 99.999999
+    lat: 50.905765, lng: -1.391138
 }
 
 let eplTSpurs = {
-    lat: 99.999999, lng: 99.999999
+    lat: 51.604100, lng: -0.066446
 }
 
 let eplWestBrom = {
-    lat: 99.999999, lng: 99.999999
+    lat: 52.508960, lng: -1.963938
 }
 
 let eplWestHam = {
-    lat: 99.999999, lng: 99.999999
+    lat: 51.538626, lng: -0.016449
 }
 
 let eplWolves = {
-    lat: 99.999999, lng: 99.999999
+    lat: 52.590184, lng: -2.130412
 }
 
 /* Super League*/
 
 let slrCasTig = {
-    lat: 99.999999, lng: 99.999999
+    lat: 53.729517, lng: -1.340833
 }
 
 let slrCatDra = {
-    lat: 99.999999, lng: 99.999999
+    lat: 42.722828, lng: 2.885358
 }
 
 let slrHudGia = {
-    lat: 99.999999, lng: 99.999999
+    lat: 53.654194, lng: -1.768365
 }
 
 let slrHullFC = {
-    lat: 99.999999, lng: 99.999999
+    lat: 53.746286, lng: -0.367702
 }
 
 let slrHullKR = {
-    lat: 99.999999, lng: 99.999999
+    lat: 53.754022, lng: -0.265279
 }
 
 let slrLeeRhi = {
-    lat: 99.999999, lng: 99.999999
+    lat: 53.816331, lng: -1.582257
 }
 
 let slrStHlns = {
-    lat: 99.999999, lng: 99.999999
+    lat: 53.447313, lng: -2.726213
 }
 
 let slrSalRed = {
-    lat: 99.999999, lng: 99.999999
+    lat: 53.469339, lng: -2.374945
 }
 
 let slrWakTri = {
-    lat: 99.999999, lng: 99.999999
+    lat: 53.669707, lng: -1.479360
 }
 
 let slrWarWol = {
-    lat: 99.999999, lng: 99.999999
+    lat: 53.394812, lng: -2.595800
 }
 
 let slrWigWar = {
-    lat: 99.999999, lng: 99.999999
+    lat: 53.547562, lng: -2.654000
+}
+
+/* League of Ireland */
+
+let loiBohemi = {
+    lat: 53.361751,
+    lng: -6.275308
+}
+
+let loiCorCty = {
+    lat: 51.885509,
+    lng: -8.467874
+}
+
+let loiDerCty = {
+    lat: 54.990601,
+    lng: -7.336107
+}
+
+let loiDundalk = {
+    lat: 53.998930,
+    lng: -6.416433
+}
+
+let loiShaRov = {
+    lat: 53.283324,
+    lng: -6.373823
+}
+
+let loiSliRov = {
+    lat: 54.269872,
+    lng: -8.487382
+}
+
+let loiShelbo = {
+    lat: 53.367465,
+    lng: -6.251664
+}
+
+let loiStPats = {
+    lat: 53.340936,
+    lng: -6.316799
+}
+
+let loiUCDAFC = {
+    lat: 53.310339,
+    lng: -6.228142
+}
+
+let loiWaterf = {
+    lat: 52.245894,
+    lng: -7.125159
 }
