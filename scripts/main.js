b605e0f1f5707d2913cbef81484d6a21530808ab
Stuart Saint <stuartsaint242@gmail.com>
2020-10-10 18:12:48 +0000

    Testing: STILL not working

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,8 @@ $("div").on( "click", "button", function( event ) {
   $(event.delegateTarget ).css( "background-color", "green");
 }); */
 
-$('#slrDrop').click(function(event) {
+$( '#slrDrop' ).on( 'click', function( event ) {
     /* $(".dropdown-submenu").css("display", "none"); hides any league menus that are already open */
-    $('#slrClublist').css('display', 'none'); /* then displays the relevant league menu */
+    $('#slrClublist' ).css( 'display', 'none'); /* then displays the relevant league menu */
+    /* re-centre map based on lat and long */
 });
