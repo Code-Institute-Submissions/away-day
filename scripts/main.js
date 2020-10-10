b5050e7b01142181e2074f5ed6e7a3e78536d250
Stuart Saint <stuartsaint242@gmail.com>
2020-10-10 16:47:39 +0000

    Modify: added initial event listeners, not yet working

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,10 @@
-/* Main JS to go here*/
+/* Main JS to go here
 
 $("div").on( "click", "button", function( event ) {
   $(event.delegateTarget ).css( "background-color", "green");
+}); */
+
+$("#slrDrop").on("click", function(event) {
+    $(".dropdown-submenu").css("display", "none"); /* hides any league menus that are already open */
+    $("li #slrClublist").css("display", "block"); /* then displays the relevant league menu */
 });
