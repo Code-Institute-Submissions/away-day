8bf29663928411581b0ae7d81cf1514f2c6e741a
Stuart Saint <stuartsaint242@gmail.com>
2020-10-10 17:39:27 +0000

    Modify: JS troubleshooting

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,7 @@ $("div").on( "click", "button", function( event ) {
   $(event.delegateTarget ).css( "background-color", "green");
 }); */
 
-$("#slrDrop").on("click", function(event) {
-    $(".dropdown-submenu").css("display", "none"); /* hides any league menus that are already open */
-    $("li #slrClublist").css("display", "block"); /* then displays the relevant league menu */
+$('#slrDrop').click(function(event) {
+    /* $(".dropdown-submenu").css("display", "none"); hides any league menus that are already open */
+    $('#slrClublist').css('display', 'none'); /* then displays the relevant league menu */
 });
