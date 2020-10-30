1e48320bf5804d41ee87ef1d2c560f4a64f29641
Stuart Saint <stuartsaint242@gmail.com>
2020-10-30 00:05:02 +0000

    Cleanup to-do tasks

--- scripts/main.js
+++ scripts/main.js
@@ -5,8 +5,7 @@ $(document).ready(function() {
         $(".dropdown-submenu").removeClass("d-flex flex-wrap"); /*hides any menus that are already open */
         $("#bunClublist").addClass("d-flex flex-wrap");
         $("#bunClublist").css("display", "flex");
-        console.log("BUN SUCCESS"); /* test */
-        /* re-centre map and zoom based on lat and long */
+        console.log("BUN SUCCESS"); /* initial test */
     });
 
     $("#flcDrop").on("click",function() {
@@ -14,8 +13,7 @@ $(document).ready(function() {
         $(".dropdown-submenu").removeClass("d-flex flex-wrap"); /*hides any menus that are already open */
         $("#flcClublist").addClass("d-flex flex-wrap");
         $("#flcClublist").css("display", "flex");
-        console.log("FLC SUCCESS"); /* test */
-        /* re-centre map and zoom based on lat and long */
+        console.log("FLC SUCCESS"); /* initial test */
     });
 
     $("#eplDrop").on("click",function() {
@@ -23,8 +21,7 @@ $(document).ready(function() {
         $(".dropdown-submenu").removeClass("d-flex flex-wrap"); /*hides any menus that are already open */
         $("#eplClublist").addClass("d-flex flex-wrap");
         $("#eplClublist").css("display", "flex");
-        console.log("EPL SUCCESS"); /* test */
-        /* re-centre map and zoom based on lat and long */
+        console.log("EPL SUCCESS"); /* initial test */
     });
 
     $("#gaaDrop").on("click",function() {
@@ -32,8 +29,7 @@ $(document).ready(function() {
         $(".dropdown-submenu").removeClass("d-flex flex-wrap"); /*hides any menus that are already open */
         $("#gaaClublist").addClass("d-flex flex-wrap");
         $("#gaaClublist").css("display", "flex");
-        console.log("GAA SUCCESS"); /* test */
-        /* re-centre map and zoom based on lat and long */
+        console.log("GAA SUCCESS"); /* initial test */
     });
 
     $("#loiDrop").on("click",function() {
@@ -41,8 +37,7 @@ $(document).ready(function() {
         $(".dropdown-submenu").removeClass("d-flex flex-wrap"); /*hides any menus that are already open */
         $("#loiClublist").addClass("d-flex flex-wrap");
         $("#loiClublist").css("display", "flex");
-        console.log("LOI SUCCESS"); /* test */
-        /* re-centre map and zoom based on lat and long */
+        console.log("LOI SUCCESS"); /* initial test */
     });
 
     $("#nhlDrop").on("click",function() {
@@ -50,8 +45,7 @@ $(document).ready(function() {
         $(".dropdown-submenu").removeClass("d-flex flex-wrap"); /*hides any menus that are already open */
         $("#nhlClublist").addClass("d-flex flex-wrap");
         $("#nhlClublist").css("display", "flex");
-        console.log("NHL SUCCESS"); /* test */
-        /* re-centre map and zoom based on lat and long */
+        console.log("NHL SUCCESS"); /* initial test */
     });
     
     $("#slrDrop").on("click",function() {
@@ -59,7 +53,6 @@ $(document).ready(function() {
         $(".dropdown-submenu").removeClass("d-flex flex-wrap"); /*hides any menus that are already open */
         $("#slrClublist").addClass("d-flex flex-wrap");
         $("#slrClublist").css("display", "flex");
-        console.log("SLR SUCCESS"); /* test */
-        /* re-centre map and zoom based on lat and long */
+        console.log("SLR SUCCESS"); /* initial test */
     });
 });
