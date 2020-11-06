c9b866d7974a17a5a9e68af8389533d78bc2163a
Stuart Saint <stuartsaint242@gmail.com>
2020-11-06 11:11:41 +0000

    Addition: hover animation

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,58 +1,63 @@
 /* Drop-Downs */
 $(document).ready(function() {
-    $("#bunDrop").on("click",function() {
-        $(".dropdown-submenu").css("display", "none") /*hides any menus that are already open */
-        $(".dropdown-submenu").removeClass("d-flex flex-wrap"); /*hides any menus that are already open */
-        $("#bunClublist").addClass("d-flex flex-wrap");
-        $("#bunClublist").css("display", "flex");
-        console.log("BUN SUCCESS"); /* initial test */
+    /* Change of icon when cursor enters a league dropdown */
+    $("#bunDrop").on("mouseenter",function() {
+        $("#bun-icon").attr("src", "images/dropdowns/bun-8a2be2.png");
     });
 
-    $("#flcDrop").on("click",function() {
-        $(".dropdown-submenu").css("display", "none") /*hides any menus that are already open */
-        $(".dropdown-submenu").removeClass("d-flex flex-wrap"); /*hides any menus that are already open */
-        $("#flcClublist").addClass("d-flex flex-wrap");
-        $("#flcClublist").css("display", "flex");
-        console.log("FLC SUCCESS"); /* initial test */
+    $("#eplDrop").on("mouseenter",function() {
+        $("#epl-icon").attr("src", "images/dropdowns/epl-8a2be2.png");
     });
 
-    $("#eplDrop").on("click",function() {
-        $(".dropdown-submenu").css("display", "none") /*hides any menus that are already open */
-        $(".dropdown-submenu").removeClass("d-flex flex-wrap"); /*hides any menus that are already open */
-        $("#eplClublist").addClass("d-flex flex-wrap");
-        $("#eplClublist").css("display", "flex");
-        console.log("EPL SUCCESS"); /* initial test */
+    $("#flcDrop").on("mouseenter",function() {
+        $("#flc-icon").attr("src", "images/dropdowns/flc-8a2be2.png");
     });
 
-    $("#gaaDrop").on("click",function() {
-        $(".dropdown-submenu").css("display", "none") /*hides any menus that are already open */
-        $(".dropdown-submenu").removeClass("d-flex flex-wrap"); /*hides any menus that are already open */
-        $("#gaaClublist").addClass("d-flex flex-wrap");
-        $("#gaaClublist").css("display", "flex");
-        console.log("GAA SUCCESS"); /* initial test */
+    $("#gaaDrop").on("mouseenter",function() {
+        $("#gaa-icon").attr("src", "images/dropdowns/gaa-8a2be2.png");
     });
 
-    $("#loiDrop").on("click",function() {
-        $(".dropdown-submenu").css("display", "none") /*hides any menus that are already open */
-        $(".dropdown-submenu").removeClass("d-flex flex-wrap"); /*hides any menus that are already open */
-        $("#loiClublist").addClass("d-flex flex-wrap");
-        $("#loiClublist").css("display", "flex");
-        console.log("LOI SUCCESS"); /* initial test */
+    $("#loiDrop").on("mouseenter",function() {
+        $("#loi-icon").attr("src", "images/dropdowns/loi-8a2be2.png") ;
     });
 
-    $("#nhlDrop").on("click",function() {
-        $(".dropdown-submenu").css("display", "none") /*hides any menus that are already open */
-        $(".dropdown-submenu").removeClass("d-flex flex-wrap"); /*hides any menus that are already open */
-        $("#nhlClublist").addClass("d-flex flex-wrap");
-        $("#nhlClublist").css("display", "flex");
-        console.log("NHL SUCCESS"); /* initial test */
+    $("#nhlDrop").on("mouseenter",function() {
+        $("#nhl-icon").attr("src", "images/dropdowns/nhl-8a2be2.png");
     });
+
+    $("#slrDrop").on("mouseenter",function() {
+        $("#slr-icon").attr("src", "images/dropdowns/slr-8a2be2.png");
+    });
+
+    /* Changes back to default icon upon cursor exit */
+    $("#bunDrop").on("mouseleave",function() {
+        $("#bun-icon").attr("src", "images/dropdowns/bun-ffffff.png");
+    });
+
+    $("#eplDrop").on("mouseleave",function() {
+        $("#epl-icon").attr("src", "images/dropdowns/epl-ffffff.png");
+    });
+
+    $("#flcDrop").on("mouseleave",function() {
+        $("#flc-icon").attr("src", "images/dropdowns/flc-ffffff.png");
+    });
+
+    $("#gaaDrop").on("mouseleave",function() {
+        $("#gaa-icon").attr("src", "images/dropdowns/gaa-ffffff.png");
+    });
+
+    $("#loiDrop").on("mouseleave",function() {
+        $("#loi-icon").attr("src", "images/dropdowns/loi-ffffff.png");
+    });
+
+    $("#nhlDrop").on("mouseleave",function() {
+        $("#nhl-icon").attr("src", "images/dropdowns/nhl-ffffff.png");
+    });
+
+    $("#slrDrop").on("mouseleave",function() {
+        $("#slr-icon").attr("src", "images/dropdowns/slr-ffffff.png");
+    });
+
+    /* add club crest animation down here */
     
-    $("#slrDrop").on("click",function() {
-        $(".dropdown-submenu").css("display", "none") /*hides any menus that are already open */
-        $(".dropdown-submenu").removeClass("d-flex flex-wrap"); /*hides any menus that are already open */
-        $("#slrClublist").addClass("d-flex flex-wrap");
-        $("#slrClublist").css("display", "flex");
-        console.log("SLR SUCCESS"); /* initial test */
-    });
 });
