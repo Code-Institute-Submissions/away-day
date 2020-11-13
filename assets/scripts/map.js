99ce85d66a7e00fd6bcfd9cc43da197d4311e0c5
Stuart Saint <stuartsaint242@gmail.com>
2020-11-13 10:52:44 +0000

    Removal: moved info-window-img styling to style.css

--- assets/scripts/map.js
+++ assets/scripts/map.js
@@ -1333,23 +1333,21 @@ function marker(place) {
 }
 
 function showDetails(placeResult, marker) {
-
     let placeInfowindow = new google.maps.InfoWindow();
         let rating = "None";
         let placePhoto = placeResult.photos[0].getUrl();
         if (placeResult.rating) rating = placeResult.rating;
-        placeInfowindow.setContent(
-            `<div>
-                <div style="text-transform: uppercase; color: #8e2be2"><b>${placeResult.name}</b></div>
-                <br>
-                <img src="${placePhoto}" alt="${placeResult.name}" width="200px" height="200px">
-                <br>
-                <br>
-                Rating: ${rating}
-                <br>
-                <a href="${placeResult.website}" target="_blank" style="color: #05e680">Website</a>
-                <br>
-                </div>`);
+            placeInfowindow.setContent(
+                `<div>
+                    <div style="text-transform: uppercase; color: #8e2be2"><b>${placeResult.name}</b></div>
+                    <br>
+                    <img src="${placePhoto}" alt="${placeResult.name}" id="info-window-img">
+                    <br>
+                    Rating: ${rating}
+                    <br>
+                    <a href="${placeResult.website}" target="_blank" style="color: #05e680">Website</a>
+                    <br>
+                    </div>`);
         placeInfowindow.open(marker.map, marker);
         /* needs code to close any open windows: look in section 4E of the tutorial for some ideas around adding classes then removing
         any items that contain said classes.*/
