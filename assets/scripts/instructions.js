62056f99544b54c509afcaf2bb9a6cd7f4385f5e
Stuart Saint <stuartsaint242@gmail.com>
2020-11-15 17:40:19 +0000

    Bugfix #10: Added instructions as a modal for mobile view

---
+++ assets/scripts/instructions.js
@@ -0,0 +1,13 @@
+/* Scripts for instructions interactivity */
+
+/* Displays instructions as a modal for smaller screen widths */
+$(document).ready(function() {
+    if ($(window).width() < 576) {
+        $("#instructions-modal").modal("show");
+        console.log("Instructions success");
+    }
+    $("#modal-close").on("click", function() {
+        $("#instructions-modal").modal("hide");
+        console.log("Hide instructions");
+    });
+});
